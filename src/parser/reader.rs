ca7467bf33022c77ce478d080e51fc3d2079101a
Chad Cotton <chad.cotton@outlook.com>
2025-08-30 17:37:02 -0500

    Generalized reader.

--- src/parser/reader.rs
+++ src/parser/reader.rs
@@ -1,47 +1,45 @@
 #[derive(Clone)]
-pub struct Reader {
-    source: Vec<char>,
+pub struct Reader<S> {
+    source: Vec<S>,
     cursor: usize,
 }
 
-impl Reader {
-    pub fn init(source: &str) -> Self {
-        let chars = source.chars().to_owned();
-        let buf = chars.collect::<Vec<char>>();
-        Reader { source: buf, cursor: 0 }
+impl<S> Reader<S> where S: Clone {
+    pub fn init(source: Vec<S>) -> Self {
+        Reader { source, cursor: 0 }
     }
 
-    pub fn peek_single(&self) -> Option<char> {
+    pub fn peek_single(&self) -> Option<S> {
         match self.peek(1) {
-            Some(char) => Some(char[0]),
+            Some(char) => Some(char[0].clone()),
             None => None,
         }
     }
 
-    pub fn peek(&self, n: usize) -> Option<&[char]> {
+    pub fn peek(&self, n: usize) -> Option<Vec<S>> {
         if self.cursor + n - 1 >= self.source.len() {
             return None;
         }
 
-        Some(&self.source[self.cursor..self.cursor + n])
+        Some(self.source[self.cursor..self.cursor + n].to_vec())
     }
 
-    pub fn next_single(&mut self) -> Option<char> {
+    pub fn next_single(&mut self) -> Option<S> {
         let next = match self.next(1) {
-            Some(char) => Some(char[0]),
+            Some(char) => Some(char[0].clone()),
             None => None,
         };
         next
     }
 
-    pub fn next(&mut self, n: usize) -> Option<&[char]> {
+    pub fn next(&mut self, n: usize) -> Option<Vec<S>> {
         if self.cursor + n - 1 >= self.source.len() {
             return None;
         }
 
         let next = Some(&self.source[self.cursor..self.cursor + n]);
         self.cursor += n;
-        next
+        next.map(|slice| slice.to_vec())
     }
 
     #[inline]
@@ -61,16 +59,16 @@ mod tests {
 
     #[test]
     fn test_reader() {
-        let mut reader = Reader::init("javascript");
+        let mut reader = Reader::init("javascript".as_bytes().to_vec());
         let next = reader.next(4);
         assert!(next.is_some());
-        assert_eq!(next.unwrap().into_iter().collect::<String>(), String::from("java"));
+        assert_eq!(String::from_utf8(next.unwrap()).unwrap(), "java");
         let peek = reader.peek(6);
         assert!(peek.is_some());
-        assert_eq!(peek.unwrap().into_iter().collect::<String>(), String::from("script"));
+        assert_eq!(String::from_utf8(peek.unwrap()).unwrap(), "script");
         let next = reader.next(6);
         assert!(next.is_some());
-        assert_eq!(next.unwrap().into_iter().collect::<String>(), String::from("script"));
+        assert_eq!(String::from_utf8(next.unwrap()).unwrap(), "script");
         let peek = reader.peek(1);
         assert!(peek.is_none());
         let next = reader.next(1);
